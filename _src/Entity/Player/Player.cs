22e85f0e9f84ca6e3851354b6afa2699b1d91539
Nooh Alavi <noohalavi@gmail.com>
2021-02-17 14:59:55 -0500

    Added Vertical Player Movement

--- _src/Entity/Player/Player.cs
+++ _src/Entity/Player/Player.cs
@@ -9,10 +9,18 @@ public class Player : KinematicBody2D
     public override void _PhysicsProcess(float delta)
     {
         GetInput();
+        velocity = velocity.Normalized() * movementSpeed;
+        velocity = MoveAndSlide(velocity);
     }
 
     private void GetInput()
     {
-        velocity.x = (Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left")) * movementSpeed;
+        velocity.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        velocity.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+    }
+
+    private void Jump()
+    {
+        
     }
 }
