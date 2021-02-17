7b774cfd22806938add1dbd95a504de08c281b54
Nooh Alavi <noohalavi@gmail.com>
2021-02-17 14:40:26 -0500

    put project in _src folder

--- _src/Entity/Player/Player.cs
+++ _src/Entity/Player/Player.cs
@@ -4,6 +4,7 @@ using System;
 public class Player : KinematicBody2D
 {
     [Export] public Vector2 velocity;
+    [Export] public float movementSpeed;
 
     public override void _PhysicsProcess(float delta)
     {
