9140c1daa8cf0fee253c39d4558570b69e9ca822
Nooh Alavi <noohalavi@gmail.com>
2021-02-17 15:09:07 -0500

    Added Gravity

--- _src/Entity/Player/Player.cs
+++ _src/Entity/Player/Player.cs
@@ -5,17 +5,27 @@ public class Player : KinematicBody2D
 {
     [Export] public Vector2 velocity = Vector2.Zero;
     [Export] public float movementSpeed = 200f;
+    [Export] public float gravityForce = 200f;
 
     public override void _PhysicsProcess(float delta)
     {
         GetInput();
+        
+        //Implement Gravity
+        if (IsOnFloor()) velocity.y = 0f;
+        else velocity.y += gravityForce;
+
         velocity = velocity.Normalized() * movementSpeed;
-        velocity = MoveAndSlide(velocity);
+        velocity = MoveAndSlide(velocity, Vector2.Up);
     }
 
     private void GetInput()
     {
         velocity.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        if (Input.IsActionPressed("jump"))
+        {
+            Jump();
+        }
     }
 
     private void Jump()
