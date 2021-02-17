eee88862e71217d641b5bbc45abcba027c88a3fb
Nooh Alavi <noohalavi@gmail.com>
2021-02-17 14:51:05 -0500

    Added Player

--- _src/Entity/Player/Player.cs
+++ _src/Entity/Player/Player.cs
@@ -3,11 +3,16 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    [Export] public Vector2 velocity;
-    [Export] public float movementSpeed;
+    [Export] public Vector2 velocity = Vector2.Zero;
+    [Export] public float movementSpeed = 200f;
 
     public override void _PhysicsProcess(float delta)
     {
-        
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        velocity.x = (Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left")) * movementSpeed;
     }
 }
