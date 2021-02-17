6bc9e3bc6068561368a8fb21e5317c8316875ded
Nooh Alavi <noohalavi@gmail.com>
2021-02-17 17:59:08 -0500

    Added Jumping

--- _src/Entity/Player/Player.cs
+++ _src/Entity/Player/Player.cs
@@ -5,23 +5,31 @@ public class Player : KinematicBody2D
 {
     [Export] public Vector2 velocity = Vector2.Zero;
     [Export] public float movementSpeed = 200f;
-    [Export] public float gravityForce = 200f;
+    [Export] public float gravity = 20f;
+    [Export] public float jumpForce = 400f;
 
     public override void _PhysicsProcess(float delta)
-    {
-        GetInput();
-        
+    {   
         //Implement Gravity
-        if (IsOnFloor()) velocity.y = 0f;
-        else velocity.y += gravityForce;
+        if (IsOnFloor())
+        {
+            velocity.y = 0f;
+        }
+        else velocity.y += gravity;
 
-        velocity = velocity.Normalized() * movementSpeed;
+        GetInput();
+
+        //Calculates Movement
+        // velocity = velocity.Normalized() * movementSpeed;
+        velocity.x *= movementSpeed;
         velocity = MoveAndSlide(velocity, Vector2.Up);
     }
 
     private void GetInput()
     {
         velocity.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+
+        //Call Jump() if player clicks W, Up arrow, spacebar, or A on controller
         if (Input.IsActionPressed("jump"))
         {
             Jump();
@@ -30,6 +38,8 @@ public class Player : KinematicBody2D
 
     private void Jump()
     {
-
+        if (!IsOnFloor()) return;
+        velocity.y = 0;
+        velocity.y -= jumpForce;
     }
 }
