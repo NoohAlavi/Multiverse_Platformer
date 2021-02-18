ab2699583ebe64f02af1a3370d00a21378af9c82
Nooh Alavi <noohalavi@gmail.com>
2021-02-18 11:35:14 -0500

    Bug looks towards player

--- _src/Entity/Bug/Bug.cs
+++ _src/Entity/Bug/Bug.cs
@@ -7,11 +7,28 @@ public class Bug : KinematicBody2D
     [Export] public Vector2 velocity = Vector2.Zero;
     [Export] public float movementSpeed = 100f;
     [Export] public float gravity = 200f;
+
+    private Sprite sprite;
     
     public override void _Ready()
     {
         targetPlayer = GetNode<Player>("../../../Player");
         GD.Print(targetPlayer);
+
+        sprite = GetNode<Sprite>("Sprite");
+    }
+
+    public override void _Process(float delta)
+    {
+        //Make the sprite face the player's direction
+        if (velocity.x > 0f)
+        {
+            sprite.FlipH = true;
+        }
+        else if (velocity.x < 0f)
+        {
+            sprite.FlipH = false;
+        }
     }
 
     public override void _PhysicsProcess(float delta)
